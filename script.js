2295e16cc8d78a5b3ebfb50fe3247b331e957e5f
Tome <marchigomes@gmail.com>
2024-05-09 14:16:42 -0300

    goind

--- script.js
+++ script.js
@@ -1,68 +1,94 @@
-const playground = document.querySelector(".playground")
-const snake = document.querySelector(".snake")
-const body = document.querySelector("body")
+const playground = document.querySelector(".playground");
+const snake = document.querySelector(".snake");
+const body = document.querySelector("body");
 
-let lastK = ""
+let lastK = "";
 
-let snakes = []
-let ycord = 3
-let xcord = 3
+let snakes = [];
+let ycord = 3;
+let xcord = 3;
 
+playground.appendChild(snake);
+snake.style.gridColumn = 3;
+snake.style.gridRow = 3;
 
-playground.appendChild(snake)
-snake.style.gridColumn = 3
-snake.style.gridRow = 3
-
-
-let intervalId; 
+let intervalId;
 
 document.body.addEventListener("keydown", (event) => {
-    clearInterval(intervalId); 
-    switch(event.key){
-        case 'w':
-            if(lastK !== "s" && lastK !== "w"){
-                lastK = "w"
-                intervalId = setInterval(() => {
-                    ycord--;
-                }, 100);
-            }
-            break;
-        
-        case "s":
-            if(lastK !== "w" && lastK !== "s"){
-                lastK = "s"
-                intervalId = setInterval(() => {
-                    ycord++;
-                }, 100);
-            }
-            break;
+  clearInterval(intervalId);
+  switch (event.key) {
+    case "w":
+      if (lastK !== "s" && lastK !== "w") {
+        lastK = "w";
+        ycord--;
+        intervalId = setInterval(() => {
+          ycord--;
+        }, 150);
+      }
+      break;
 
-        case "d":
-            if(lastK !== "d" && lastK !== "a"){
-                lastK = "d"
-                    intervalId = setInterval(() => {
-                        xcord++;
-                    }, 100);
-                }
-                break;
-            
-        case "a":
-            if(lastK !== "a" && lastK !== "d"){
-                lastK = "a"
-                clearInterval(intervalId); 
-                    intervalId = setInterval(() => {
-                        xcord--;
-                    }, 100);
-                }
-                break;
+    case "s":
+      if (lastK !== "w" && lastK !== "s") {
+        lastK = "s";
+        ycord++;
+        intervalId = setInterval(() => {
+          ycord++;
+        }, 150);
+      }
+      break;
 
-    }
+    case "d":
+      if (lastK !== "d" && lastK !== "a") {
+        lastK = "d";
+        xcord++;
+        intervalId = setInterval(() => {
+          xcord++;
+        }, 150);
+      }
+      break;
+
+    case "a":
+      if (lastK !== "a" && lastK !== "d") {
+        lastK = "a";
+        xcord--;
+        clearInterval(intervalId);
+        intervalId = setInterval(() => {
+          xcord--;
+        }, 150);
+      }
+      break;
+  }
 });
 
 
+setInterval(() => {
+    snake.style.gridColumn = xcord;
+    snake.style.gridRow = ycord;
+  }, 10);
 
-setInterval(()=>{
-    snake.style.gridColumn = xcord
-    snake.style.gridRow = ycord
-},100)
 
+
+
+
+let tailR = []
+let tailC = []
+
+function getCLocation() {
+   
+    const computedStyle = window.getComputedStyle(snake);
+
+
+    const gridColumn = computedStyle.gridColumn;
+
+  
+    return gridColumn;
+}
+
+function getRLocation(){
+
+    let computedStyle = window.getComputedStyle(snake);
+    
+    let gridRow = computedStyle.gridRow;
+
+    return gridRow;
+}

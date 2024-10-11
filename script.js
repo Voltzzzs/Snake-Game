f5ccc2e3023ac25ca147bde9a41d071bfe550012
Voltz <marchigomes@gmail.com>
2024-10-11 10:22:38 -0300

    manzana

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 for (i = 0; i < 121; i++){
     let tile = document.createElement('div')
     let row;
@@ -10,7 +11,8 @@ for (i = 0; i < 121; i++){
     document.querySelector(".playground").appendChild(tile);
 }
 
-
+const tiles = document.querySelectorAll(".tile")
+let tailSize = 0;
 let headRow = 5;
 let headColumn = 5;
 
@@ -35,57 +37,96 @@ setColor()
 let keypress;
 
 
+
 document.querySelector("body").addEventListener("keypress",(event)=>{
        keypress = event.key
        
  })
+
+
+function verifyCatch(){
+    if(headIndex === randomNum){
+        tailSize++
+        manzana()
+    }
+}
+
+function deColorTail(){
+    let bunda = tail.shift()
+    if(tailSize > 0 ){
+        tiles[bunda].style.backgroundColor = "brown"
+    }
+}
+
+
+
 
  function moving (){
 
 
     if(keypress === "w"){
      headToRemove = headIndex
+     tail.push(headToRemove)
      headRow--
      headIndex = (headRow * 11) + headColumn
     }else if(keypress === "s"){
         headToRemove = headIndex
+        tail.push(headToRemove)
         headRow++
         headIndex = (headRow * 11) + headColumn
        } else if(keypress === "a"){
         headToRemove = headIndex
+        tail.push(headToRemove)
         headColumn--
         headIndex = (headRow * 11) + headColumn
        } else if(keypress === "d"){
         headToRemove = headIndex
+        tail.push(headToRemove)
         headColumn++
         headIndex = (headRow * 11) + headColumn
        }
 
-    if(headColumn > 11 || headColumn < 0 || headRow < 0 || headRow > 11){
+    if(headColumn > 10 || headColumn < 0 || headRow < 0 || headRow > 10){
         document.querySelector("body").style.backgroundColor = "black"
     }
-       setColor()
-    console.log(headColumn)
-    console.log(headIndex)
-    console.log(headToRemove)
+        verifyCatch()
+        setColor()
+
+
+
+
+
+    console.log(tailSize)
      
  }
 
 
  function setColor (rowNum, columnNum){
-    let tiles = document.querySelectorAll(".tile")
   
 
-    tiles[headToRemove].style.backgroundColor = "red"
+    tiles[headToRemove].style.backgroundColor = "brown"
     tiles[headIndex].style.backgroundColor = 'black'
-    
+
+
+     deColorTail()
+
     tail.forEach((element)=>{
-        tiles[element].style.backgroundColor = 'black'
+       tiles[element].style.backgroundColor = "black"
     })
-   
+    
 
 }
 
- setInterval(moving,200);
+ setInterval(moving,160);
 
- 
+
+let randomNum;
+
+function manzana (){
+    randomNum = Math.round(Math.random()*120)
+    tiles[randomNum].style.backgroundColor = "blue"
+
+    return randomNum;
+} 
+
+manzana()
